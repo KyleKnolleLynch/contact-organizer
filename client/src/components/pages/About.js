adbc1b21087fea828dba0a7febde47bba202b70e
Kyle Lynch <kylelynch104@gmail.com>
2019-06-26 17:41:21 -0600

    begin front-end, complete navbar

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className="m-1">
+        This is a full stack React app for organizing and keeping contacts.
+      </p>
+      <p className="text-center bg-dark"><strong>Version: 1.0.0</strong></p>
+    </div>
+  )
+}
+
+export default About;
